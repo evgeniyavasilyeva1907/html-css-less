65cad06641f32a0b085f21464d5c037fac87449f
evgeniya <novenkaya1907@mail.ru>
2020-08-24 15:34:22 +0300

    finish

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-var mixer = mixitup('.container-filter')
+var mixer = mixitup('.container-filter');
+$(document).ready(function(){
+    $('a[href^="#"]').click(function(){
+        var target = $(this).attr('href');
+        $('html,body').animate({
+            scrollTop: $(target).offset().top
+        },500);
+        return false;
+    })
+})
